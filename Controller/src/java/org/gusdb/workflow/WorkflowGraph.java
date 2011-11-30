d502588d00ed772705dae4cc86dfb89afe420011
Steve Fischer <sfischer@upenn.edu>
2011-11-30 02:46:39 +0000

    improve err message for steps that need db cleanup
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/ReFlow/trunk@9998 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/workflow/WorkflowGraph.java
+++ Controller/src/java/org/gusdb/workflow/WorkflowGraph.java
@@ -598,7 +598,24 @@ public class WorkflowGraph<T extends WorkflowStep> extends WorkflowXmlContainer<
   // remove from the db all READY or ON_DECK steps
   void removeReadyStepsFromDb() throws SQLException, FileNotFoundException,
       IOException {
+    
     String workflowStepTable = getWorkflow().getWorkflowStepTable();
+    String workflowStepTrackingTable = getWorkflow().getWorkflowStepTrackingTable();
+    String sql = "select s.name"
+	+ " from " + workflowStepTable + " s, "
+	+ workflowStepTrackingTable + "t"
+        + " where s.workflow_id = " + workflow.getId()
+        + " and s.workflow_step_id = t.workflow_step_id and s.state = 'READY'";
+
+    Set<String> stepNamesInDb = workflowGraph.getStepNamesInDb(sql);
+    if (stepNamesInDb.size() != 0) {
+	String msg = "The following steps are READY but have rows in "
+	    + workflowStepTrackingTable
+	    + ". This means that they ran, failed and were set to ready witout being cleaned up in the database.  Refer to each step's step.err file for instructions on how to clean it up in the database. ";
+	for(String s : stepNamesInDb) msg += ("  " + s + nl);
+	Utilities.error(msg);
+    }
+
     String sql = "delete from " + workflowStepTable + " where workflow_id = "
         + workflow.getId() + " and (state = 'READY' or state = 'ON_DECK')";
     workflow.executeSqlUpdate(sql);
@@ -606,12 +623,16 @@ public class WorkflowGraph<T extends WorkflowStep> extends WorkflowXmlContainer<
 
   Set<String> getStepNamesInDb() throws SQLException, FileNotFoundException,
       IOException {
-    String workflowStepTable = getWorkflow().getWorkflowStepTable();
-    Set<String> stepsInDb = new HashSet<String>();
-
     String sql = "select name" + " from " + workflowStepTable
         + " where workflow_id = " + workflow.getId()
         + " order by depth_first_order";
+    return getStepNamesInDb(sql);
+  }
+
+  Set<String> getStepNamesInDb(String sql) throws SQLException, FileNotFoundException,
+      IOException {
+    String workflowStepTable = getWorkflow().getWorkflowStepTable();
+    Set<String> stepsInDb = new HashSet<String>();
 
     Statement stmt = null;
     ResultSet rs = null;
