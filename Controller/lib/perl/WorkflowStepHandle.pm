e7f0c743d404186d01b8b5e579923ac91eb79add
Steve Fischer <sfischer@upenn.edu>
2012-08-30 00:11:20 +0000

    improve email message on mail alerts
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/ReFlow/trunk@10960 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/lib/perl/WorkflowStepHandle.pm
+++ Controller/lib/perl/WorkflowStepHandle.pm
@@ -573,7 +573,7 @@ sub maybeSendAlert {
     print SENDMAIL "To: $maillist\n";
     print SENDMAIL "From: reflow\@eupathdb.org\n";
     print SENDMAIL "Content-type: text/plain\n\n";
-    print SENDMAIL "woohoo!";
+    print SENDMAIL "woohoo!\n\n$homeDir";
     close(SENDMAIL);
 }
 
