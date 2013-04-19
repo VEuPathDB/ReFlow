a9e8cfc746b05e0f97b08b9852bfcefc1189af73
Steve Fischer <sfischer@upenn.edu>
2013-04-19 00:39:30 +0000

    lose unneeded logging message; close input streams
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/ReFlow/trunk@11960 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/java/org/gusdb/workflow/RunnableWorkflow.java
+++ Controller/src/java/org/gusdb/workflow/RunnableWorkflow.java
@@ -1,5 +1,6 @@
 package org.gusdb.workflow;
 
+import java.io.InputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -335,7 +336,8 @@ public class RunnableWorkflow extends Workflow<RunnableWorkflowStep> {
             runningCount += step.handleChangesSinceLastSnapshot(this);
             notDone |= !step.getOperativeState().equals(DONE);
         }
-        log("Number of steps running: " + runningCount);
+	// don't know why this is in here, but seems like way too much logging
+	//  log("Number of steps running: " + runningCount);
         if (!notDone) setDoneState(testOnly);
         return !notDone;
     }
@@ -486,7 +488,9 @@ public class RunnableWorkflow extends Workflow<RunnableWorkflowStep> {
         String[] cmd = { "bash", "-c", "echo $PPID" };
         Process p = Runtime.getRuntime().exec(cmd);
         p.waitFor();
-        p.getInputStream().read(bo);
+        InputStream s = p.getInputStream();
+	s.read(bo);
+	s.close();
         p.destroy();
         return new String(bo).trim();
     }
@@ -498,7 +502,9 @@ public class RunnableWorkflow extends Workflow<RunnableWorkflowStep> {
             String[] cmd = { "ls", "-l", getHomeDir() + "/kill" };
             Process p = Runtime.getRuntime().exec(cmd);
             p.waitFor();
-            p.getInputStream().read(bo);
+	    InputStream s = p.getInputStream();
+	    s.read(bo);
+	    s.close();
             p.destroy();
             String details = new String(bo).trim();
             log("Found kill file:");
