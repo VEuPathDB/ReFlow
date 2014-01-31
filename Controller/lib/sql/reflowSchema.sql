decc4147c990f43300d5723d8236b5b75b5518b2
Steve Fischer <sfischer@upenn.edu>
2014-01-31 19:03:19 +0000

    was out of date.  now has all current tables
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/ReFlow/trunk@13377 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/lib/sql/reflowSchema.sql
+++ Controller/lib/sql/reflowSchema.sql
@@ -17,22 +17,20 @@ ALTER TABLE Workflow
 ADD CONSTRAINT workflow_uniq
 UNIQUE (name, version);
 
--- This is oracle and postgresql syntax.  The name of this
--- MUST be _sq concatenated onto the name of the Workflow table.
 CREATE SEQUENCE Workflow_sq;
-
 
 -----------------------------------------------------------
 
 create table WorkflowStep (
   workflow_step_id    number(10),
   workflow_id         number(10),
-  name                varchar(200),
+  name                varchar(500),
   host_machine        varchar(30),
   process_id          number(10),
   state               varchar(30),
   state_handled       number(1),
   last_handled_time   date,
+  skipped             number(1),
   off_line            number(1),
   stop_after          number(1),
   undo_state          varchar(30),
@@ -44,7 +42,7 @@ create table WorkflowStep (
   end_time            date,
   step_class          varchar(200),
   params_digest       varchar(100),
-  depends_string      varchar(4000),
+  depends_string      clob,
   depth_first_order   number(5)
 );
 
@@ -69,8 +67,64 @@ REFERENCES WorkflowStep (workflow_step_id);
 CREATE INDEX workflow_revix0
 ON Workflow (undo_step_id, workflow_id);
 
--- This is oracle and postgresql syntax.  The name of this
--- MUST be _sq concatenated onto the name of the WorkflowStep table.
 CREATE SEQUENCE WorkflowStep_sq;
+
+
+---------------------------------------------------------------------------
+
+create table WorkflowStepAlgInvocation (
+  workflow_step_alg_inv_id number(10),
+  workflow_step_id number(10),
+  algorithm_invocation_id number(10)
+);
+
+ALTER TABLE WorkflowStepAlgInvocation
+ADD CONSTRAINT workflow_step_alg_inv_pk PRIMARY KEY (workflow_step_alg_inv_id);
+
+ALTER TABLE WorkflowStepAlgInvocation
+ADD CONSTRAINT workflow_step_alg_inv_fk1 FOREIGN KEY (workflow_step_id)
+REFERENCES WorkflowStep (workflow_step_id);
+
+CREATE INDEX WorkflowStepAlgInv_revix1
+ON WorkflowStepAlgInvocation (workflow_step_id, workflow_step_alg_inv_id);
+
+-- Optionally have a fk constraint on the alg invocation.  This is how we do it in GUS
+--ALTER TABLE WorkflowStepAlgInvocation
+--ADD CONSTRAINT workflow_step_alg_inv_fk2 FOREIGN KEY (algorithm_invocation_id)
+--REFERENCES core.AlgorithmInvocation (algorithm_invocation_id);
+
+CREATE INDEX WorkflowStepAlgInv_revix2
+ON WorkflowStepAlgInvocation (algorithm_invocation_id, workflow_step_alg_inv_id);
+
+ALTER TABLE WorkflowStepAlgInvocation
+ADD CONSTRAINT workflow_step_alg_inv_uniq
+UNIQUE (workflow_step_id, algorithm_invocation_id);
+
+CREATE SEQUENCE WorkflowStepAlgInvocation_sq;
+
+---------------------------------------------------------------------------
+
+create table WorkflowStepParamValue (
+  workflow_step_param_value_id number(10),
+  workflow_step_id number(10),
+  param_name varchar(100),
+  param_value varchar(500)
+);
+
+ALTER TABLE WorkflowStepParamValue
+ADD CONSTRAINT workflow_step_param_val_pk PRIMARY KEY (workflow_step_param_value_id);
+
+ALTER TABLE WorkflowStepParamValue
+ADD CONSTRAINT workflow_step_param_val_fk1 FOREIGN KEY (workflow_step_id)
+REFERENCES WorkflowStep (workflow_step_id);
+
+CREATE INDEX WorkflowStepParamValue_revix1
+ON WorkflowStepParamValue (workflow_step_id, workflow_step_param_value_id);
+
+ALTER TABLE WorkflowStepParamValue
+ADD CONSTRAINT workflow_step_param_value_uniq
+UNIQUE (workflow_step_id,param_name);
+
+CREATE SEQUENCE WorkflowStepParamValue_sq;
 
 exit;
