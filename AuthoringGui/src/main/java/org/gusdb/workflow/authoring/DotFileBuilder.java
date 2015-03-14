533e8abe5a31171eee88fe7285792f0952ba1118
Ryan Doherty <tech@conical.org>
2015-03-14 21:06:20 +0000

    Convert ReFlow to Maven
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/ReFlow/trunk@15682 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- AuthoringGui/src/main/java/org/gusdb/workflow/authoring/DotFileBuilder.java
+++ AuthoringGui/src/main/java/org/gusdb/workflow/authoring/DotFileBuilder.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.gusdb.fgputil.FormatUtil.NL;
+
 import org.gusdb.fgputil.FormatUtil;
 import org.gusdb.fgputil.IoUtil;
 import org.gusdb.fgputil.xml.Name;
@@ -16,75 +18,70 @@ import org.gusdb.workflow.xml.WorkflowXmlParser;
 
 public class DotFileBuilder {
 
-  private final static String NL = System.getProperty("line.separator");
-  
   private final static int MAX_CHARS_PER_LINE = 20;
 
   private static boolean _keepPath = false;
   private static String _relativeDirPath;
-  
+
   public static void main(String[] args) {
     try {
       File inputFile = parseArgs(args);
-      
-      WorkflowXmlParser<SimpleXmlNode, SimpleXmlGraph> parser =
-        new WorkflowXmlParser<SimpleXmlNode, SimpleXmlGraph>();
+
+      WorkflowXmlParser<SimpleXmlNode, SimpleXmlGraph> parser = new WorkflowXmlParser<SimpleXmlNode, SimpleXmlGraph>();
 
       SimpleXmlGraph graph = parser.parseWorkflow(SimpleXmlNode.class, SimpleXmlGraph.class,
           inputFile.getAbsolutePath(), "", false);
-      
+
       // build map of node groups and list of those not in a group
       Map<String, List<SimpleXmlNode>> groupingMap = new HashMap<String, List<SimpleXmlNode>>();
       List<SimpleXmlNode> nonGroupedNodes = new ArrayList<SimpleXmlNode>();
       for (SimpleXmlNode vertex : graph.getNodes()) {
-          String groupName = vertex.getGroupName();
-	  if (groupName == null || groupName.trim().equals("")) {
-	      nonGroupedNodes.add(vertex);
-	  }
-	  else {
-	      List<SimpleXmlNode> nodeList = groupingMap.get(groupName);
-	      if (nodeList == null) {
-		  nodeList = new ArrayList<SimpleXmlNode>();
-		  groupingMap.put(groupName, nodeList);
-	      }
-	      nodeList.add(vertex);
-	  }
+        String groupName = vertex.getGroupName();
+        if (groupName == null || groupName.trim().equals("")) {
+          nonGroupedNodes.add(vertex);
+        }
+        else {
+          List<SimpleXmlNode> nodeList = groupingMap.get(groupName);
+          if (nodeList == null) {
+            nodeList = new ArrayList<SimpleXmlNode>();
+            groupingMap.put(groupName, nodeList);
+          }
+          nodeList.add(vertex);
+        }
       }
 
       // append dot file header
-      StringBuilder output = new StringBuilder()
-        .append("digraph name {").append(NL)
-        .append("  graph [ fontsize=8, rankdir=\"TB\" ]").append(NL)
-        .append("  node [ fontsize=8, height=0, width=0, margin=0.03,0.02 ]").append(NL)
-	.append("  edge [ fontsize=8, arrowhead=open ]").append(NL);
-      
+      StringBuilder output = new StringBuilder().append("digraph name {").append(NL).append(
+          "  graph [ fontsize=8, rankdir=\"TB\" ]").append(NL).append(
+          "  node [ fontsize=8, height=0, width=0, margin=0.03,0.02 ]").append(NL).append(
+          "  edge [ fontsize=8, arrowhead=open ]").append(NL);
+
       // append vertex info for ungrouped nodes
       dumpVertexInfo(output, nonGroupedNodes);
 
       // create subgraph for each group
       int clusterNum = 0;
       for (String groupName : groupingMap.keySet()) {
-	  String clusterName = "cluster" + clusterNum; // this name MUST have "cluster" as a prefix!
-	  output.append("subgraph ").append(clusterName).append(" {").append(NL)
-	      .append("  label = \"").append(getValidGroupName(groupName)).append("\"").append(NL);
-	  dumpVertexInfo(output, groupingMap.get(groupName));
-	  output.append("}").append(NL);
-	  clusterNum++;
+        String clusterName = "cluster" + clusterNum; // this name MUST have "cluster" as a prefix!
+        output.append("subgraph ").append(clusterName).append(" {").append(NL).append("  label = \"").append(
+            getValidGroupName(groupName)).append("\"").append(NL);
+        dumpVertexInfo(output, groupingMap.get(groupName));
+        output.append("}").append(NL);
+        clusterNum++;
       }
-      
+
       // append dependency info
       for (SimpleXmlNode vertex : graph.getNodes()) {
         String nodeLabel = formatNodeLabel(vertex.getBaseName());
         for (Name dependency : vertex.getDependsNames()) {
-          output
-            .append("  ").append(formatNodeLabel(dependency.getName())).append(" -> ")
-            .append(nodeLabel).append(NL);
+          output.append("  ").append(formatNodeLabel(dependency.getName())).append(" -> ").append(nodeLabel).append(
+              NL);
         }
       }
 
       // append dot file footer
       output.append("}").append(NL);
-      
+
       // dump to stdout
       System.out.println(output.toString());
     }
@@ -96,50 +93,47 @@ public class DotFileBuilder {
 
   private static void dumpVertexInfo(StringBuilder output, List<SimpleXmlNode> nodeList) {
     for (SimpleXmlNode vertex : nodeList) {
-        String nodeLabel = formatNodeLabel(vertex.getBaseName());
-        output
-	    .append("  ")
-	    .append(nodeLabel)
-	    .append(" [ shape=rectangle");
-        if (vertex.getGroupName() != null && vertex.getGroupName().length() > 0) {
-	    output.append(", group=").append(getValidGroupName(vertex.getGroupName()));
-        }
-        if (vertex.getSubgraphXmlFileName() != null) {
-	    if (vertex.getSubgraphXmlFileName().startsWith("$$")) {
-		// add url, but make sure it points only to the file (html)
-		output.append(", color=green");
-	    }
-	    else {
-		// add url, but make sure it points only to the file (html)
-		String htmlFileRelPath = vertex.getSubgraphXmlFileName().replace(".xml", ".html");
-		if (_keepPath) {
-		    // count '/'s and subtract one for number of directories to backtrack
-		    String[] dirs = _relativeDirPath.split("/");
-		    String backtrack = "";
-		    for (int i=1; i < dirs.length; i++) backtrack += "../";
-		    output.append(", URL=\"" + backtrack + htmlFileRelPath + "\", color=blue, penwidth=2");
-		}
-		else {
-		    File htmlFile = new File(htmlFileRelPath);
-		    output.append(", URL=\"" + htmlFile.getName() + "\", color=blue, penwidth=2");
-		}
-	    }
+      String nodeLabel = formatNodeLabel(vertex.getBaseName());
+      output.append("  ").append(nodeLabel).append(" [ shape=rectangle");
+      if (vertex.getGroupName() != null && vertex.getGroupName().length() > 0) {
+        output.append(", group=").append(getValidGroupName(vertex.getGroupName()));
+      }
+      if (vertex.getSubgraphXmlFileName() != null) {
+        if (vertex.getSubgraphXmlFileName().startsWith("$$")) {
+          // add url, but make sure it points only to the file (html)
+          output.append(", color=green");
         }
         else {
-	    output.append(", color=black");
+          // add url, but make sure it points only to the file (html)
+          String htmlFileRelPath = vertex.getSubgraphXmlFileName().replace(".xml", ".html");
+          if (_keepPath) {
+            // count '/'s and subtract one for number of directories to backtrack
+            String[] dirs = _relativeDirPath.split("/");
+            String backtrack = "";
+            for (int i = 1; i < dirs.length; i++)
+              backtrack += "../";
+            output.append(", URL=\"" + backtrack + htmlFileRelPath + "\", color=blue, penwidth=2");
+          }
+          else {
+            File htmlFile = new File(htmlFileRelPath);
+            output.append(", URL=\"" + htmlFile.getName() + "\", color=blue, penwidth=2");
+          }
         }
-        output.append(" ]").append(NL);
+      }
+      else {
+        output.append(", color=black");
+      }
+      output.append(" ]").append(NL);
     }
   }
 
   private static String getValidGroupName(String rawName) {
-    return rawName.replaceAll("-","_");
+    return rawName.replaceAll("-", "_");
   }
 
   /**
-   * Base name passed in is a camel-case string representing a name; it may
-   * also contain underscores and dashes, which must be trimmed and/or
-   * formatted nicely.  The first letter should also be capitalized if it
+   * Base name passed in is a camel-case string representing a name; it may also contain underscores and
+   * dashes, which must be trimmed and/or formatted nicely. The first letter should also be capitalized if it
    * isn't already.
    * 
    * @param baseName
@@ -180,7 +174,8 @@ public class DotFileBuilder {
   }
 
   private static void printUsageAndDie() {
-    System.err.println("USAGE: java " + DotFileBuilder.class.getName() + " [--backtrack <relativePath>] <workflowXmlFile>");
+    System.err.println("USAGE: java " + DotFileBuilder.class.getName() +
+        " [--backtrack <relativePath>] <workflowXmlFile>");
     System.exit(1);
   }
 }
