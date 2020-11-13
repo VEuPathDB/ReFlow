VEuPathDB/ReFlow

commit 72d40d1e7eb48f78f252569fbceaf1a9ed161303

Fix bug for git mode: both nextflow pull and nextflow run are within bsub

StepClasses/lib/perl/RunAndMonitorNextflow.pm
@@ -89,15 +89,22 @@ sub runAndMonitor {
 	return 1 if $self->_checkClusterTaskLogForDone($logFile, $user, $transferServer);
 
 	my $nextflowCmd = "nextflow run $nextflowWorkflow -with-trace -c $clusterNextflowConfigFile -resume >$nextflowStdoutFile 2>&1";
+        if($isGit){
+          $nextflowCmd = "nextflow pull $nextflowWorkflow; $nextflowCmd";
+        }
 
-        my $fullCommand = $isGit ? "nextflow pull $nextflowWorkflow; $nextflowCmd" : $nextflowCmd;
+        # prepend slash to ;, >, and & so that the command is submitted whole
+        $nextflowCmd =~ s{([;>&])}{\\$1}g;
 
-	my $submitCmd = $self->getNodeClass()->getQueueSubmitCommand($queue, $fullCommand);
+	my $submitCmd = $self->getNodeClass()->getQueueSubmitCommand($queue, $nextflowCmd);
 
+
+        # wrap in a login shell to allow nextflow to be user installed
 	my $cmd = "cd $workingDir; $submitCmd ";
+        $cmd = "/bin/bash -login -c \"$cmd\"";
 
 	# do the submit on submit server, and capture its output
-        my $jobInfo = $self->_runSshCmdWithRetries(0, "/bin/bash -login -c \"$cmd\"", "", 1, 0, $user, $submitServer, "");
+        my $jobInfo = $self->_runSshCmdWithRetries(0, $cmd, "", 1, 0, $user, $submitServer, "");
 
 	$self->error("Did not get jobInfo back from command:\n $cmd") unless $jobInfo;
 

commit eb37ef32837506d80cf78c9f6b9d7544ccf4b4c1

revert to 2bdcb42ad7a7adaf81655d649641ba2bf24ba2f7, 72d40d1e7eb48f78f252569fbceaf1a9ed161303 introduced a bug when running nextflow on PMACS

StepClasses/lib/perl/RunAndMonitorNextflow.pm
@@ -89,22 +89,15 @@ sub runAndMonitor {
 	return 1 if $self->_checkClusterTaskLogForDone($logFile, $user, $transferServer);
 
 	my $nextflowCmd = "nextflow run $nextflowWorkflow -with-trace -c $clusterNextflowConfigFile -resume >$nextflowStdoutFile 2>&1";
-        if($isGit){
-          $nextflowCmd = "nextflow pull $nextflowWorkflow; $nextflowCmd";
-        }
 
-        # prepend slash to ;, >, and & so that the command is submitted whole
-        $nextflowCmd =~ s{([;>&])}{\\$1}g;
+        my $fullCommand = $isGit ? "nextflow pull $nextflowWorkflow; $nextflowCmd" : $nextflowCmd;
 
-	my $submitCmd = $self->getNodeClass()->getQueueSubmitCommand($queue, $nextflowCmd);
+	my $submitCmd = $self->getNodeClass()->getQueueSubmitCommand($queue, $fullCommand);
 
-
-        # wrap in a login shell to allow nextflow to be user installed
 	my $cmd = "cd $workingDir; $submitCmd ";
-        $cmd = "/bin/bash -login -c \"$cmd\"";
 
 	# do the submit on submit server, and capture its output
-        my $jobInfo = $self->_runSshCmdWithRetries(0, $cmd, "", 1, 0, $user, $submitServer, "");
+        my $jobInfo = $self->_runSshCmdWithRetries(0, "/bin/bash -login -c \"$cmd\"", "", 1, 0, $user, $submitServer, "");
 
 	$self->error("Did not get jobInfo back from command:\n $cmd") unless $jobInfo;
 