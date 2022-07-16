8e46bb0b46573459258ecc224e4d2fb085543a18
Wei Li <weili1@pennmedicine.upenn.edu>
2022-07-16 08:51:27 -0400

    For nextflow steps, use -C instead of -c to avoid taking from anything besides the specified config

--- StepClasses/lib/perl/RunAndMonitorNextflow.pm
+++ StepClasses/lib/perl/RunAndMonitorNextflow.pm
@@ -89,7 +89,9 @@ sub runAndMonitor {
 	# first see if by any chance we are already done (would happen if somehow the flow lost track of the job)
 	return 1 if $self->_checkClusterTaskLogForDone($logFile, $user, $transferServer);
 
-	my $nextflowCmd = "nextflow run $nextflowWorkflow -with-trace -c $clusterNextflowConfigFile -resume >$nextflowStdoutFile 2>&1";
+	#my $nextflowCmd = "nextflow run $nextflowWorkflow -with-trace -c $clusterNextflowConfigFile -resume >$nextflowStdoutFile 2>&1";
+    #use "-C" instead of "-c" to avoid taking from anything besides the specified config
+my $nextflowCmd = "nextflow -C $clusterNextflowConfigFile run $nextflowWorkflow -with-trace -resume >$nextflowStdoutFile 2>&1";    
         if($isGit){
           $nextflowCmd = "nextflow pull $nextflowWorkflow; $nextflowCmd";
         }
